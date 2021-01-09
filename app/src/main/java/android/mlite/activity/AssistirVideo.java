a75d4f2373a172da2c52f6a4b06a7b0383e9bf32
Rafael Parente <rafaelp.dev@gmail.com>
2021-01-09 08:40:39 -0300

    AssistirVideo activity label, keep screen on while playing and completionControls icons fixed

--- app/src/main/java/android/mlite/activity/AssistirVideo.java
+++ app/src/main/java/android/mlite/activity/AssistirVideo.java
@@ -5,19 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.mlite.R;
+import android.mlite.component.CustomVideoView;
 import android.mlite.pojo.Aula;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
-import android.widget.VideoView;
 
 public class AssistirVideo extends Activity {
 
     private Aula aula;
 
-    private VideoView videoView;
+    private CustomVideoView videoView;
 
     private LinearLayout completionControlsLayout;
 
@@ -27,29 +28,47 @@ public class AssistirVideo extends Activity {
         setContentView(R.layout.activity_assistir_video);
         videoView = findViewById(R.id.videoView);
         completionControlsLayout = findViewById(R.id.completionControlsLayout);
+        configureVideo();
 
         Intent intent = getIntent();
         aula = intent.getParcelableExtra("videoName");
         executarVideo(aula.getVideo());
     }
 
-    private void executarVideo(String videoName) {
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + videoName);
-        videoView.setVideoURI(uri);
+    private void configureVideo() {
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
-        videoView.start();
 
+        videoView.setPlayPauseListener(new CustomVideoView.PlayPauseListener() {
+            @Override
+            public void onPlay() {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+
+            @Override
+            public void onPause() {
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             public void onCompletion(MediaPlayer mp) {
                 completionControlsLayout.setVisibility(View.VISIBLE);
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         });
+    }
+
+    private void executarVideo(String videoName) {
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/raw/" + videoName);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
     }
 
     public void reiniciarVideo(View view) {
         completionControlsLayout.setVisibility(View.INVISIBLE);
         videoView.seekTo(0);
+        videoView.requestFocus();
         videoView.start();
     }
 
